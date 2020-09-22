5a805372b2896bfd8bef21d073597c0de92f13c8
Amber-Pittman <amber@amberpittman.com>
2020-09-22 11:20:58 -0400

    Added click event to switch from dead to live and vice versa

--- code.js
+++ code.js
@@ -31,6 +31,8 @@ function createTable() {
             cell.setAttribute("id", i + "_" + j);
             // Add CSS class to cell for styling
             cell.setAttribute("class", "dead")
+            // Add click handler for switching from Dead to Alive cells
+            cell.onclick = cellClickHandler;
             // Add the tCol to the row (cell is the tCols)
             tr.appendChild(cell);
         }
@@ -41,6 +43,21 @@ function createTable() {
 
     // Add table to grid container
     gridContainer.appendChild(table);
+}
+
+// Click handler to change Dead to Alive and vice versa
+function cellClickHandler() {
+    // Get element class using getAttribute
+    let classes = this.getAttribute("class");
+    // Check to see in the class contains the string live
+    // If classes contains the string "live", then the result will be 0 because the index of the first letter, "l" is at position 0 in the classes string. If the string in classes was "not live", then the result would be 4. So we check for > -1 because the result can be 0 or greater.
+    if (classes.indexOf("live") > -1) {
+        // if live cell, switch it to dead class attr
+        this.setAttribute("class", "dead");
+    } else {
+        // if the selected cell is not live, switch it to live class attr
+        this.setAttribute("class", "live")
+    }
 }
 
 
