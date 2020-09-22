78c3b2332b48639336051183f79d786edc401485
Amber-Pittman <amber@amberpittman.com>
2020-09-22 17:00:04 -0400

    Initialized both grids and set state

--- code.js
+++ code.js
@@ -3,11 +3,40 @@ let columns = 40;
 
 // keep track of whether or not the game is playing
 let playing = false;
+// Set state of 2 grids - double-buffering
+let grid = new Array(rows);
+let nextGrid = new Array(rows)
+
+// Initialize both grids that will add an array to each item in both grids
+function initializeGrids() {
+    // since both grids are the same size, we can do this in one single loop
+    for (let i = 0; i < rows; i++) {
+        /* loops over every row and add another array with the num of columns to that row in both grids. */
+        grid[i] = new Array(columns);
+        nextGrid[i] = new Array(columns);
+    }
+}
+
+/* Make sure both grid arrays have a 0 in every cell - every cell starts in the "dead" state. We're using 0 for dead and 1 for alive. Instead of doing this in the initializeGrids function, we're using the resetGrids function to clear out the 2 arrays when you click the clear button. */
+function resetGrids() {
+    // loop over every row and column in both grids & sets both i and j to 0
+    // resets both grids to all dead cells
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < rows; j++) {
+            grid[i][j] = 0;
+            nextGrid[i][j] = 0;
+        }
+    }
+}
 
 // initialize
 function initialize() {
     // immediately create the table
     createTable();
+    // call the grids so we'll have two 2-D arrays
+    initializeGrids();
+    // make all cells "dead"
+    resetGrids();
     // get buttons onto the page as soon as the page is loaded
     setupControlButtons();
 }
@@ -34,6 +63,7 @@ function createTable() {
             // The <td> tag defines a standard data cell in an HTML table.
             let cell = document.createElement("td");
             // each cell needs a unique ID - adding ID by using row number and the column number
+            // the underscore makes it easier to read row_col location
             cell.setAttribute("id", i + "_" + j);
             // Add CSS class to cell for styling
             cell.setAttribute("class", "dead")
@@ -53,6 +83,11 @@ function createTable() {
 
 // Click handler to change Dead to Alive and vice versa
 function cellClickHandler() {
+    /* We click on a cell. With the split func, it separates the location by row and column - creating an array of 2 values. We use these variables to determine the row and the column for the cell in the model that corresponds to the cell in the view. */
+    let rowCol = this.id.split("_");
+    let rows = rowCol[0];
+    let columns = rowCol[1]
+    
     // Get element class using getAttribute
     let classes = this.getAttribute("class");
     // Check to see in the class contains the string live
@@ -60,9 +95,14 @@ function cellClickHandler() {
     if (classes.indexOf("live") > -1) {
         // if live cell, switch it to dead class attr
         this.setAttribute("class", "dead");
+        // live cell becomes dead by setting state to 0
+        grid[rows][columns] = 0;
     } else {
         // if the selected cell is not live, switch it to live class attr
         this.setAttribute("class", "live")
+        // clicked on dead cell to make it live by setting state to 1
+        grid[rows][columns] = 1;
+        
     }
 }
 
